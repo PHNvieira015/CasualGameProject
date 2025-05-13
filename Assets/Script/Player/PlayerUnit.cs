dab4c20078d009c88c3ca83edd2fa2146459f862
Peter <pedro.hvieira@al.infnet.edu.br>
2025-05-13 06:35:41 -0300

    Player Energy done

--- Assets/Script/Player/PlayerUnit.cs
+++ Assets/Script/Player/PlayerUnit.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class PlayerUnit : Unit
 {
     public int MaxEnergy;
 
-    public int CurrentEnergy;
+    public int CurrentEnergy
+    {
+        get
+        {
+            return _currentEnergy;
+        }
+        set
+        {
+            //modify/add/control
+            _currentEnergy = value;
+            UpdateEnergyMeter();
+        }
+    }
+    [SerializeField]
+    int _currentEnergy;
 
     public int DrawAmount;
     
     public int MaxCards;
+
+    Text _energyMeter;
+    
+    void awake()
+    {
+        _energyMeter = GameObject.Find("Canvas/EnergyMeter Text (TMP").GetComponent<Text>();
+    }
 
     public override IEnumerator Recover()
     {
@@ -18,6 +40,10 @@ public class PlayerUnit : Unit
         int cardsToDraw = Mathf.Min(MaxCards - CardsController.Instance.Hand.Cards.Count, DrawAmount);
         yield return StartCoroutine(CardsController.Instance.Draw(DrawAmount));
         //yield return null;
+    }
+    void UpdateEnergyMeter()
+    {
+    _energyMeter.text = string.Format("{0}/{1}", CurrentEnergy, MaxEnergy);
     }
 
 }
