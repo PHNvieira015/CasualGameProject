efcbbbcd204527767387796a65b57bf6a1ec88ec
Peter <pedro.hvieira@al.infnet.edu.br>
2025-06-10 23:07:07 -0300

    Making UI
    Working on Interfaces

--- Assets/Script/UI/Menu/MenuController.cs
+++ Assets/Script/UI/Menu/MenuController.cs
@@ -11,16 +11,30 @@ public class MenuController : MonoBehaviour
         Loading,
         CharacterSelect,
         Settings,
-        QuitConfirm
+        QuitConfirm,
+        Reward,
+        CardSelection,
+        Defeat,
+        CombatStart,
+        DeckGallery,
+        DiscardPile,
+        DrawPile
     }
     public Screens currentScreen;
 
     [SerializeField] private GameObject MainMenu;
     [SerializeField] private GameObject loadingScreen;
-    [SerializeField] private GameObject characterSelect;   
+    [SerializeField] private GameObject characterSelect;
     [SerializeField] private GameObject settings;
     [SerializeField] private GameObject quitConfirm;
     [SerializeField] private GameObject messageScreen;
+    [SerializeField] private GameObject RewardScreen;
+    [SerializeField] private GameObject CardSelection;
+    [SerializeField] private GameObject DefeatScreen;
+    [SerializeField] private GameObject CombatStartScreen;
+    [SerializeField] private GameObject DeckGalleryScreen;
+    [SerializeField] private GameObject DiscardPileGalleryScreen;
+    [SerializeField] private GameObject DrawPileGalleryScreen;
     [SerializeField] private TMP_Text messageTXT;
 
     [HideInInspector] public Screens TargetScreen;
@@ -37,13 +51,18 @@ public class MenuController : MonoBehaviour
         settings.SetActive(false);
         quitConfirm.SetActive(false);
         messageScreen.SetActive(false);
+        RewardScreen.SetActive(false);
+        DefeatScreen.SetActive(false);
+        CombatStartScreen.SetActive(false);
+        DeckGalleryScreen.SetActive(false);
+        DiscardPileGalleryScreen.SetActive(false);
+        DrawPileGalleryScreen.SetActive(false);
 
         // Activate the desired screen based on the passed screen
         switch (_screen)
         {
             case Screens.None:
-                MainMenu.SetActive(true);
-                Debug.Log("Main screen activated");
+                // No screens active
                 break;
             case Screens.Main:
                 MainMenu.SetActive(true);
@@ -60,6 +79,27 @@ public class MenuController : MonoBehaviour
                 break;
             case Screens.QuitConfirm:
                 quitConfirm.SetActive(true);
+                break;
+            case Screens.Reward:
+                RewardScreen.SetActive(true);
+                break;
+            case Screens.CardSelection:
+                CardSelection.SetActive(true);
+                break;
+            case Screens.Defeat:
+                DefeatScreen.SetActive(true);
+                break;
+            case Screens.CombatStart:
+                CombatStartScreen.SetActive(true);
+                break;
+            case Screens.DeckGallery:
+                DeckGalleryScreen.SetActive(true);
+                break;
+            case Screens.DiscardPile:
+                DiscardPileGalleryScreen.SetActive(true);
+                break;
+            case Screens.DrawPile:
+                DrawPileGalleryScreen.SetActive(true);
                 break;
         }
 
@@ -91,4 +131,4 @@ public class MenuController : MonoBehaviour
         Application.Quit();
         Debug.Log("Game Closed");
     }
- }
+}
