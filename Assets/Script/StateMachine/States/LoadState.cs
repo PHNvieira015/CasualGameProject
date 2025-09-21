11db3ae2665b32afa3e292319ee3cc0022a7dc90
Peter <pedro.hvieira@al.infnet.edu.br>
2025-09-21 11:51:01 -0300

    Card Start Shuffle

--- Assets/Script/StateMachine/States/LoadState.cs
+++ Assets/Script/StateMachine/States/LoadState.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq; // Add this for LINQ functionality
 
 public class LoadState : State
 {
@@ -10,18 +11,32 @@ public class LoadState : State
         yield return StartCoroutine(InitializeUnits());
         yield return StartCoroutine(InitializeRelics());
         StartCoroutine(WaitThenChangeState<TurnBeginState>());
-
     }
+
     IEnumerator InitializeDeck()
     {
+        // Create a list to store all instantiated cards
+        List<Card> instantiatedCards = new List<Card>();
+
         foreach (Card card in CardsController.Instance.PlayerDeck.Cards)
         {
             Card newCard = Instantiate(card, Vector3.zero, Quaternion.identity, CardsController.Instance.DrawPile.Holder);
-            CardsController.Instance.DrawPile.AddCard(newCard);
+            instantiatedCards.Add(newCard);
         }
-        yield return new WaitForSeconds(CardHolder.CardMoveDuration);  
+
+        // Shuffle the instantiated cards
+        instantiatedCards = ShuffleCards(instantiatedCards);
+
+        // Add shuffled cards to draw pile
+        foreach (Card card in instantiatedCards)
+        {
+            CardsController.Instance.DrawPile.AddCard(card);
+        }
+
+        yield return new WaitForSeconds(CardHolder.CardMoveDuration);
         CardsController.Instance.DrawPile.SetInitialRotation();
     }
+
     IEnumerator InitializeUnits()
     {
         machine.Units = new Queue<Unit>();
@@ -30,8 +45,8 @@ public class LoadState : State
             machine.Units.Enqueue(unit);
         }
         yield return null;
-        //Debug.Log(machine.Units.Count);
     }
+
     IEnumerator InitializeRelics()
     {
         foreach (Card card in CardsController.Instance.PlayerRelics.Cards)
@@ -43,5 +58,28 @@ public class LoadState : State
         CardsController.Instance.RelicHolder.SetInitialRotation();
     }
 
+    // Fisher-Yates shuffle algorithm for proper randomization
+    private List<Card> ShuffleCards(List<Card> cards)
+    {
+        System.Random rng = new System.Random();
+        int n = cards.Count;
 
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            Card value = cards[k];
+            cards[k] = cards[n];
+            cards[n] = value;
+        }
+
+        return cards;
+    }
+
+    // Alternative using LINQ (simpler but less control)
+    private List<Card> ShuffleCardsLINQ(List<Card> cards)
+    {
+        System.Random rng = new System.Random();
+        return cards.OrderBy(card => rng.Next()).ToList();
+    }
 }
