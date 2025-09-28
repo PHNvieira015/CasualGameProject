76cbf6c0f618bba55bac56b8a1d35699fc87c87e
Peter <pedro.hvieira@al.infnet.edu.br>
2025-09-28 08:58:56 -0300

    Card Highlight WIP
    Almost fixed, no idea how to solve the canva sprioirty..

--- Assets/Script/CardTooltip.cs
+++ Assets/Script/CardTooltip.cs
@@ -1,20 +1,37 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 using System.Collections;
 
 public class CardTooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private GameObject tooltipPanel;
+    [Header("Objects to Hide")]
+    [SerializeField] private GameObject backObject;
+    [SerializeField] private GameObject frontObject;
+
+    [Header("Objects to Show")]
+    [SerializeField] private GameObject cardHighlight;
     [SerializeField] private float showDelay = 0.3f;
+
+    [Header("Source Components")]
+    [SerializeField] private TextMeshProUGUI source_CardName_Text;
+    [SerializeField] private UnityEngine.UI.Image source_Art;
+    [SerializeField] private TextMeshProUGUI source_Description;
+    [SerializeField] private TextMeshProUGUI source_EnergyCost_Text;
+
+    [Header("Highlight Components")]
+    [SerializeField] private TextMeshProUGUI highlight_CardName_Text;
+    [SerializeField] private UnityEngine.UI.Image highlight_Art;
+    [SerializeField] private TextMeshProUGUI highlight_Description;
+    [SerializeField] private TextMeshProUGUI highlight_EnergyCost_Text;
 
     private bool _isHovering;
     private Coroutine _showCoroutine;
 
     void Awake()
     {
-        // Hide tooltip on start
-        if (tooltipPanel != null)
-            tooltipPanel.SetActive(false);
+        // Ensure initial state is correct
+        ResetToDefaultState();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -26,7 +43,7 @@ public class CardTooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     public void OnPointerExit(PointerEventData eventData)
     {
         _isHovering = false;
-        HideTooltip();
+        ResetToDefaultState();
 
         if (_showCoroutine != null)
         {
@@ -39,21 +56,108 @@ public class CardTooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     {
         yield return new WaitForSeconds(showDelay);
 
-        if (_isHovering && tooltipPanel != null)
+        if (_isHovering)
         {
-            tooltipPanel.SetActive(true);
+            ShowTooltipState();
         }
     }
 
-    private void HideTooltip()
+    private void ShowTooltipState()
     {
-        if (tooltipPanel != null)
-            tooltipPanel.SetActive(false);
+        // Hide back object
+        if (backObject != null)
+            backObject.SetActive(false);
+
+        // Hide all child objects of front object (but keep front object active)
+        if (frontObject != null)
+        {
+            foreach (Transform child in frontObject.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        // Show highlight
+        if (cardHighlight != null)
+            cardHighlight.SetActive(true);
+
+        // Copy all components from source to highlight
+        CopyComponentsToHighlight();
+    }
+
+    private void CopyComponentsToHighlight()
+    {
+        // Copy CardName_Text
+        if (source_CardName_Text != null && highlight_CardName_Text != null)
+        {
+            highlight_CardName_Text.text = source_CardName_Text.text;
+        }
+
+        // Copy Art
+        if (source_Art != null && highlight_Art != null)
+        {
+            highlight_Art.sprite = source_Art.sprite;
+            highlight_Art.color = source_Art.color;
+        }
+
+        // Copy Description
+        if (source_Description != null && highlight_Description != null)
+        {
+            highlight_Description.text = source_Description.text;
+        }
+
+        // Copy EnergyCost_Text
+        if (source_EnergyCost_Text != null && highlight_EnergyCost_Text != null)
+        {
+            highlight_EnergyCost_Text.text = source_EnergyCost_Text.text;
+        }
+    }
+
+    private void ResetToDefaultState()
+    {
+        // Show back object
+        if (backObject != null)
+            backObject.SetActive(true);
+
+        // Show all child objects of front object
+        if (frontObject != null)
+        {
+            foreach (Transform child in frontObject.transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+
+        // Hide highlight
+        if (cardHighlight != null)
+            cardHighlight.SetActive(false);
     }
 
     void OnDisable()
     {
-        HideTooltip();
+        ResetToDefaultState();
         _isHovering = false;
+
+        if (_showCoroutine != null)
+        {
+            StopCoroutine(_showCoroutine);
+            _showCoroutine = null;
+        }
+    }
+
+    // Optional: Public method to update source components if card data changes during runtime
+    public void UpdateCardData(string cardName, Sprite art, string description, string energyCost)
+    {
+        if (source_CardName_Text != null)
+            source_CardName_Text.text = cardName;
+
+        if (source_Art != null)
+            source_Art.sprite = art;
+
+        if (source_Description != null)
+            source_Description.text = description;
+
+        if (source_EnergyCost_Text != null)
+            source_EnergyCost_Text.text = energyCost;
     }
 }
